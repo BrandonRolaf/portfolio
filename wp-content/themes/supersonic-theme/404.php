aa578cdb1cb26bdcc66c05f6a6c4032651e4097d
Brandon Rolaf <brandon041412@gmail.com>
2022-05-31 08:56:24 +0200

    check

---
+++ wp-content/themes/supersonic-theme/404.php
@@ -0,0 +1,6 @@
+<?php
+// The template to use when the URL could not be resolved to an existing page
+get_header(); ?>
+<h1>Pagina niet gevonden.</h1>
+<p>Klik hier om terug te gaan naar de hoofdpagina.</p>
+<?php get_footer(); ?>
